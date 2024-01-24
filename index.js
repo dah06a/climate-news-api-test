d9f6c95e2b3504fb51cffdc35ee583efbab36859
David Henry <dah06a@acu.edu>
2024-01-23 20:09:12 -0600

    Reverted project back to basic structure

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const axios = require('axios');
 const cheerio = require('cheerio');
-const serverless = require('serverless-http');
 
 const app = express();
 const PORT = process.env.PORT || 8000;
